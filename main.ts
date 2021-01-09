ee033b6943488552cb450cc96ac3b72b05f85b5f
mieslep <mieslep@users.noreply.github.com>
2021-01-09 11:28:50 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+function tanDegrees (num: number) {
+    return Math.tan(num / Math.PI / 180)
+}
+function sinDegrees (num: number) {
+    return Math.sin(num / Math.PI / 180)
+}
+function cosDegrees (num: number) {
+    return Math.cos(num / Math.PI / 180)
+}
+function atan2Degrees (num: number) {
+    return Math.atan2(num / Math.PI / 180, 0)
+}
